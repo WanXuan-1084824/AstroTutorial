0746b48ee923a4c3e5bfeed0c4e5f51e65c9d263
WanXuan-1084824 <wanxuanlin2005@gmail.com>
2026-09-07 10:35:58 +0200

    RSS installeren & RSS-feed gemaakt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-import partytown from '@astrojs/partytown';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()]
+  site: "https://wxastrotutorial.netlify.app/",
 });
